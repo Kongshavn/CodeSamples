2d28d7d599046c20d25aadb735bd5dc92394dc19
Andreas <andreas@kongshavn.com>
2021-10-05 14:48:39 +0200

    Added client credentials flow sample

---
+++ Authentication/Client credentials/Client credentials/ClientCredentials.Application/Models/ClientAuthenticationRequestModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ClientCredentials.Application.Models
+{
+    public class ClientAuthenticationRequestModel
+    {
+
+        public ClientAuthenticationRequestModel(string tokenEndpoint, string clientId, string clientSecret, List<string> scopes)
+        {
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            Scopes = scopes;
+            TokenEndpoint = tokenEndpoint;
+        }
+
+        private string ClientId { get; }
+        private string ClientSecret { get; }
+        private string TokenEndpoint { get; }
+        private List<string> Scopes { get; }
+
+        public HttpRequestMessage GetRequest()
+        {
+            var scopes = "";
+
+            Scopes.ForEach(s => scopes += $"{s} ");
+
+            var content = new StringContent($"grant_type=client_credentials&client_id={ClientId}&client_secret={HttpUtility.UrlEncode(ClientSecret)}&{scopes}",
+                Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            return new HttpRequestMessage(HttpMethod.Post, TokenEndpoint)
+            {
+                Content = content
+            };
+        }
+    }
+}
---
+++ Authentication/Client credentials/Client credentials/ClientCredentials.Application/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ClientCredentials.Application.Models;
+
+namespace ClientCredentials.Application
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Remember to inject with DI
+            var httpClient = new HttpClient();
+
+            var request = new ClientAuthenticationRequestModel("{TokenEndpoint}",
+                "{ClientId}", "{ClientSecret}", new List<string>() { "{ListOfScopes" });
+
+            var jsonResponse = await httpClient.SendAsync(request.GetRequest());
+
+            if (jsonResponse.IsSuccessStatusCode)
+            {
+                var response = JsonSerializer.Deserialize<ClientAuthenticationResponseModel>(await jsonResponse.Content.ReadAsStringAsync());
+                Console.WriteLine(response.AccessToken);
+            }
+            else
+                Console.WriteLine("Failed to retrieve token");
+        }
+    }
+}
